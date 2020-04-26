1ed63e6f8b0f4c6c49f4a0d72d875e0f9c85efff
Vinodh N <vinodhnagarajan@outlook.com>
2020-04-25 23:25:51 -0700

    Added a simple animation for header

--- main.js
+++ main.js
@@ -2,17 +2,38 @@ console.log("Hello!")
 function smoothTransitions() {
   var scrollPos = $(document).scrollTop();
   var targetOpacity = 1;
-  console.log(1000-scrollPos)
   $('#hero').css({
-      'opacity': (1000-scrollPos)/1000
+      'opacity': (1300-scrollPos)/1000
   });
-  $('#about').css({
-    'opacity': (scrollPos)/1000
-});
 };
+
+function animateActiveSection(){
+  var navigationSections = $('section');
+  var currentSections = $('.menu, #mobile-nav');
+
+  $(window).scroll(function(){
+    var currentPos = $(document).scrollTop() + 10;
+
+    navigationSections.each(function(){
+      var top = $(this).offset().top;
+      var bottom = top + $(this).outerHeight();
+      
+      if(currentPos >= top && currentPos <= bottom){
+        if(currentPos <= bottom){
+          currentSections.find('li').removeClass('active');
+        }
+        currentSections.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (currentPos < 200) {
+        $(".menu ul:first li:first").addClass('active');
+      }
+    })
+  })
+}
 
 $(function(){
   $(window).scroll(function() {
       smoothTransitions();
+      animateActiveSection();
   });
 });
